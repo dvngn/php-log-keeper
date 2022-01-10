dddd624e55f0a0f5c7d1d2398399efd68e118830
devengine <cloud@devengine.ru>
2022-01-10 05:51:09 +0300

    Fix regular expression in LocalLogFileRepository

--- src/Repositories/LocalLogFileRepository.php
+++ src/Repositories/LocalLogFileRepository.php
@@ -21,7 +21,7 @@ class LocalLogFileRepository implements LogFileRepository
                 return false;
             }
 
-            if (!preg_match("#.+-\d{4}-\d{2}-\d{2}.log#", $file['path'])) {
+            if (!preg_match("#^.+-\d{4}-\d{2}-\d{2}.log$#", $file['path'])) {
                 return false;
             }
 
